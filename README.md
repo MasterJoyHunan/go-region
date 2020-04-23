5b3992d3af56841bac647e54817dddcc615ef587
Master Joy <386442876@qq.com>
2020-04-23 10:49:22 +0800

    添加延迟爬取

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 ### 使用爬虫爬取国家统计局省市区编码,并存储MYSQL
 
-[国家统计局省市区统计地址](http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm)
+* [国家统计局省市区统计地址](http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm)
+
+注意：国家统计局官网有反爬虫系统，爬取速度不能过快，否则会出现请求重定向，导致爬取失败
+所以添加如下参数：
+```go
+// 限制速度
+if err := c.Limit(&colly.LimitRule{
+    DomainGlob:  "*", 
+    Parallelism: 2,
+    Delay:       200 * time.Millisecond, 
+    RandomDelay: 5 * time.Second,
+}); err != nil {
+    fastmysql.Logger.Panic("set colly limit error :", err)
+}
+```
+这样导致的后果也很明显，没有错误，但是爬取速度也变的非常慢。好在这是一劳永逸的工作，爬取之后就完事了。
+项目是爬取2019年的数据，如需修改，修改爬取地址（页面结构都是一样的，无需修改 OnHtml 回调）
+```go
+c.Visit("http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/index.html")
+```
--- main.go
+++ main.go
@@ -5,8 +5,14 @@ import (
 	"github.com/MasterJoyHunan/fastmysql"
 	"github.com/gocolly/colly"
 	"golang.org/x/text/encoding/simplifiedchinese"
+	"math/rand"
 	"strings"
+	"sync"
+	"time"
 )
+
+var tryAgain = make(map[string]int)
+var lock sync.WaitGroup
 
 func main() {
 	flag.Parse()
@@ -15,17 +21,32 @@ func main() {
 		colly.Async(true),
 	)
 
-	//fastmysql.Db.Exec("truncate region")
+	// 限制速度
+	if err := c.Limit(&colly.LimitRule{
+		DomainGlob:  "*",
+		Parallelism: 2,
+		Delay:       200 * time.Millisecond,
+		RandomDelay: 5 * time.Second,
+	}); err != nil {
+		fastmysql.Logger.Panic("set colly limit error :", err)
+	}
+
+	fastmysql.Db.Exec("truncate region")
+
+	// 随机Agent
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", RandomString())
+	})
 
 	// 发生错误
-	c.OnError(func(_ *colly.Response, err error) {
-		fastmysql.Logger.Error(err)
+	c.OnError(func(rep *colly.Response, err error) {
+		lock.Add(1)
+		go ReVisit(rep.Request)
 	})
 
 	// 爬取省级
 	c.OnHTML(".provincetr td", func(e *colly.HTMLElement) {
 		text, _ := simplifiedchinese.GBK.NewDecoder().Bytes([]byte(e.Text))
-
 		e.Text = string(text)
 		cityLink := e.ChildAttr("a", "href")
 
@@ -40,13 +61,12 @@ func main() {
 
 	// 爬取市级
 	c.OnHTML(".citytr", func(e *colly.HTMLElement) {
-
 		code := e.DOM.Children().First().Text()
 		gbkName := e.DOM.Children().Last().Text()
 		link, _ := e.DOM.Children().First().Find("a").Attr("href")
 		name, _ := simplifiedchinese.GBK.NewDecoder().Bytes([]byte(gbkName))
 		region := Region{
-			Id:       strings.Trim(code, "0"),
+			Id:       code[:4],
 			ParentId: code[:2],
 			Name:     string(name),
 		}
@@ -56,13 +76,12 @@ func main() {
 
 	// 爬取县级
 	c.OnHTML(".countytr", func(e *colly.HTMLElement) {
-
 		code := e.DOM.Children().First().Text()
 		gbkName := e.DOM.Children().Last().Text()
 		link, _ := e.DOM.Children().First().Find("a").Attr("href")
 		name, _ := simplifiedchinese.GBK.NewDecoder().Bytes([]byte(gbkName))
 		region := Region{
-			Id:       strings.Trim(code, "0"),
+			Id:       code[:6],
 			ParentId: code[:4],
 			Name:     string(name),
 		}
@@ -72,12 +91,11 @@ func main() {
 
 	// 爬取乡镇级
 	c.OnHTML(".towntr", func(e *colly.HTMLElement) {
-
 		code := e.DOM.Children().First().Text()
 		gbkName := e.DOM.Children().Last().Text()
 		name, _ := simplifiedchinese.GBK.NewDecoder().Bytes([]byte(gbkName))
 		region := Region{
-			Id:       strings.Trim(code, "0"),
+			Id:       code[:9],
 			ParentId: code[:6],
 			Name:     string(name),
 		}
@@ -86,8 +104,33 @@ func main() {
 	})
 
 	// 开始爬取
-	//c.Visit("http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/index.html")
-	c.Visit("http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/14/09/140923.html")
-
+	c.Visit("http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/index.html")
+	//c.Visit("http://www.bb.vcc.qq")
 	c.Wait()
+	lock.Wait()
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandomString() string {
+	b := make([]byte, rand.Intn(10)+10)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func ReVisit(r *colly.Request) {
+	defer lock.Done()
+	url := r.URL.String()
+	_, ok := tryAgain[url]
+	if ok {
+		//	if res >= 3 {
+		//		fastmysql.Logger.Error(url, "重试3次后,无法连接")
+		return
+		//	}
+	}
+	tryAgain[url]++
+	time.Sleep(6 * time.Minute)
+	r.Visit(url)
 }
